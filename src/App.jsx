1ef3fbbd64a2f24d5ee716b00c66fdf8c10d0ff1
Ihor Poliakov <piv2003@gmail.com>
2023-06-25 23:04:17 +0200

    fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { Component } from 'react';
-// import { nanoid } from 'nanoid';
-// import { AppBox } from 'components/App.styled';
+// import { Component } from 'react';
+// // import { nanoid } from 'nanoid';
+// // import { AppBox } from 'components/App.styled';
 
-export class App extends Component {
-  //   state = {
-  //     contacts: [
-  //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //     ],
-  //     filter: '',
-  //   };
-}
+// export class App extends Component {
+//   //   state = {
+//   //     contacts: [
+//   //       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   //       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   //       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   //       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//   //     ],
+//   //     filter: '',
+//   //   };
+// }
+
+// import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+// class Counter extends Component {
+//   static defaultProps = {
+//     step: 1,
+//   };
+
+//   render() {
+//     const { step } = this.props;
+
+//     return (
+//       <div>
+//         <span>0</span>
+//         <button type="button">Increment by {step}</button>
+//         <button type="button">Decrement by {step}</button>
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<Counter step={5} />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Counter extends Component {
+  static defaultProps = {
+    step: 1,
+  };
+
+  render() {
+    const { step } = this.props;
+
+    return (
+      <div>
+        <span>0</span>
+        <button type="button">Increment by {step}</button>
+        <button type="button">Decrement by {step}</button>
+      </div>
+    );
+  }
+}
