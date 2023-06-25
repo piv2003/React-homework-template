bbf3f1021bd65aa9e0c8f1ba4a088d74e7aa1621
Ihor Poliakov <piv2003@gmail.com>
2023-06-25 22:57:06 +0200

    Preparation

--- src/components/ContactsList/ContactsList.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
-`;
